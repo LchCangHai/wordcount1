456ab6f480a4d635e8709e806e9dcbb68a089206
林沧海 <1067407157@qq.com>
2021-03-04 11:23:12 +0800

    写好获取每行单词的测试用函数功能

--- 221801227_林沧海/src/getWord.cpp
+++ 221801227_林沧海/src/getWord.cpp
@@ -27,4 +27,29 @@ void fun::getWord(string str, map<string, int> &mymap) {
 
 }
 
+//测试用函数
+map<string, int> fun::getWordTest(string str) {
+    regex words_regex("[a-zA-Z]{4}[a-za-z0-9]*"); // 判断单词的正则表达式
+    map<string, int> mymap;
+    auto words_begin = sregex_iterator(
+        str.begin(),
+        str.end(),
+        words_regex);
+    auto words_end = sregex_iterator();
 
+    for (sregex_iterator k = words_begin; k != words_end; ++k) {
+        smatch match = *k;
+        string match_str = match.str();//获取单词
+        //////////////将单词放进map统计数量/////////////////
+        map<string, int>::iterator iter1;
+        iter1 = mymap.find(match_str);
+        if (iter1 == mymap.end()) {
+            pair<string, int> value(match_str, 1);
+            mymap.insert(value);
+        }
+        else {
+            mymap[match_str]++;
+        }
+    }
+    return mymap;
+}
