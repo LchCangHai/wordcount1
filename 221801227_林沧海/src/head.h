30bc6f2893705902abbca04103c86d5ba85cfb6b
林沧海 <1067407157@qq.com>
2021-03-04 11:21:52 +0800

    写好计算每行单词数量的功能

--- 221801227_林沧海/src/head.h
+++ 221801227_林沧海/src/head.h
@@ -6,3 +6,11 @@
 #include <map>
 #include <regex>
 using namespace std;
+
+
+class fun {
+public:
+	int countWord(string str); //计算一行（一个字符串中）的单词数
+	void getWord(string str, map<string, int>& mymap);
+	map<string, int> getWordTest(string str);
+};
