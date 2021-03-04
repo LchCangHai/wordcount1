3bf1db8909df41db698a611d46600423eced6cf6
林沧海 <1067407157@qq.com>
2021-03-04 12:23:28 +0800

    写了文件的读取和使用了单词数和获取单词的函数

--- 221801227_林沧海/src/Main.cpp
+++ 221801227_林沧海/src/Main.cpp
@@ -1,6 +1,28 @@
 #include "head.h"
 
 int main() {
+    FILE* point;      //文件指针
+    char buffer[500]; //读出文件中的一行
+    char filename[50];//读取文件路径
+    char outFile[50];//写入结果的文件路径
+    fun myfun;
+
+    int charCnt = 0,
+        wordCnt = 0,
+        lineCnt = 0;
+
+    cin >> filename >> outFile;
+
+    if ((point = fopen(filename, "rb")) == NULL) {
+        perror(filename);
+        return NULL;
+    }
+    while (fgets(buffer, 500, point) != NULL) {
+        string str = buffer;
+        ++lineCnt;
+        charCnt += strlen(buffer);
+        wordCnt += myfun.countWord(str);
+    }
 
     return 0;
 }
