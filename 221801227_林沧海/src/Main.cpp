748952f99268ae554319da2225d189b32c4782d0
林沧海 <1067407157@qq.com>
2021-03-05 09:15:08 +0800

    修改输出样式，怎加map的key排序

--- 221801227_林沧海/src/Main.cpp
+++ 221801227_林沧海/src/Main.cpp
@@ -1,12 +1,4 @@
 #include "head.h"
-typedef pair<string, int> PAIR;
-
-// 对map中value值进行排序
-struct CmpByValue {
-    bool operator()(const PAIR& lhs, const PAIR& rhs) {
-        return rhs.second < lhs.second;
-    }
-};
 
 int main() {
     char buffer[500]; //读出文件中的一行
@@ -26,22 +18,22 @@ int main() {
     //myfun.showWord(); //输出所有单词及其出现次数
 
     // 前3行
-    cout << "行数为：" << lineCnt << endl;
-    cout << "字符数为：" << charCnt << endl;
-    cout << "单词数为：" << wordCnt << endl;
-
+    cout << "characters:" << charCnt << endl;
+    cout << "words:" << wordCnt << endl;
+    cout << "lines:" << lineCnt << endl;
+    
     vector<PAIR> mymapvec(myfun.mymap.begin(), myfun.mymap.end());
     sort(mymapvec.begin(), mymapvec.end(), CmpByValue());
     int length1 = mymapvec.size();
     // 后10行
     if (length1 <= 10) {
         for (int i = 0; i != mymapvec.size(); ++i) {
-            cout << mymapvec[i].first << endl;
+            cout << mymapvec[i].first << mymapvec[i].second << endl;
         }
     }
     else {
         for (int i = 0; i < 10; ++i) {
-            cout << mymapvec[i].first << endl;
+            cout << mymapvec[i].first << mymapvec[i].second << endl;
         }
     }
     return 0;
